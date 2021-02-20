0b329970ad89a868b0701ed5005deb1713b5c320
StarTux <ths.loy@googlemail.com>
2021-02-20 15:43:58 +0100

    Deny BlockFormEvent

--- src/main/java/com/winthier/creative/CreativeListener.java
+++ src/main/java/com/winthier/creative/CreativeListener.java
@@ -23,6 +23,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockFormEvent;
 import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPistonRetractEvent;
@@ -212,6 +213,26 @@ public final class CreativeListener implements Listener {
             event.setCancelled(true);
             return;
         }
+    }
+
+    /**
+     * Called when a block is formed or spreads based on world
+     * conditions.
+     *
+     * Use BlockSpreadEvent to catch blocks that actually spread and
+     * don't just "randomly" form.
+     *
+     * Examples:
+     * - Snow forming due to a snow storm.
+     * - Ice forming in a snowy Biome like Taiga or Tundra.
+     * - Obsidian / Cobblestone forming due to contact with water.
+     * - Concrete forming due to mixing of concrete powder and water.
+     */
+    @EventHandler(priority = EventPriority.LOWEST)
+    void onBlockForm(BlockFormEvent event) {
+        BuildWorld buildWorld = plugin.getBuildWorldByWorld(event.getBlock().getWorld());
+        if (buildWorld == null) return;
+        event.setCancelled(true);
     }
 
     // Explosion
