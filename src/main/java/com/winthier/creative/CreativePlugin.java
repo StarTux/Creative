32c39c514709713b8fe36d2b8df45763545f70bf
StarTux <ths.loy@googlemail.com>
2021-09-29 20:00:30 +0200

    Outsource Areas

--- src/main/java/com/winthier/creative/CreativePlugin.java
+++ src/main/java/com/winthier/creative/CreativePlugin.java
@@ -53,7 +53,6 @@ public final class CreativePlugin extends JavaPlugin {
         getCommand("creativeadmin").setExecutor(new AdminCommand(this));
         getCommand("warp").setExecutor(new WarpCommand(this));
         getCommand("plot").setExecutor(new PlotCommand(this));
-        getCommand("area").setExecutor(new AreaCommand(this).enable());
         getServer().getPluginManager().registerEvents(new CreativeListener(this), this);
         for (Player player: getServer().getOnlinePlayers()) {
             permission.updatePermissions(player);
